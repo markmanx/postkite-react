c5657c57c65618d509d7dfaad6a0494da45356fb
Mark Mankarious <mz.mankarious@gmail.com>
2021-12-26 14:04:01 +0000

    fix: prevents including another version of react in bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,20 @@ const path = require('path')
 module.exports = {
   mode: 'production',
   target: 'web',
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
+  },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
